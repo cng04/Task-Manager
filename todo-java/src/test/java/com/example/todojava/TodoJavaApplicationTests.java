8c16b4e326eabbdcbdc29605b44aeef7095ce4d7
cng04 <calebng.ca@gmail.com>
2024-01-17 16:51:26 -0500

    first commit

---
+++ todo-java/src/test/java/com/example/todojava/TodoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
