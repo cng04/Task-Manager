0b8a5a1f529f94c9853612bca2fc45300910854f
cng04 <calebng.ca@gmail.com>
2024-01-17 23:12:15 -0500

    Fixed CORS Problem

--- todo-java/src/main/java/com/example/todojava/controller/todoAPI.java
+++ todo-java/src/main/java/com/example/todojava/controller/todoAPI.java
@@ -13,6 +13,7 @@ import com.example.todojava.database.entity.Task;
 import ch.qos.logback.core.rolling.helper.IntegerTokenConverter;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @Slf4j
 @RequestMapping("/todo")
